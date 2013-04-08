24fb41060687524eaa03217ed2e62560bcd66c5a
fogus <mefogus@gmail.com>
2013-04-08 15:30:08 -0400

    Shutdown hook

--- src/leiningen/simpleton.clj
+++ src/leiningen/simpleton.clj
@@ -1,6 +1,17 @@
-(ns leiningen.simpleton)
+(ns leiningen.simpleton
+  (import [java.io File]
+          [com.sun.net.httpserver HttpHandler HttpExchange HttpServer]
+          [java.net InetSocketAddress HttpURLConnection]
+          [java.io IOException FilterOutputStream]))
+
+(.addShutdownHook
+ (Runtime/getRuntime)
+ (Thread. #(println "Shutting down Simpleton...")))
 
 (defn simpleton
   "I don't do a lot."
-  [project & args]
-  (println "Hi!"))
+  [project & [port :as args]]
+  (println "Starting server on port " port)
+  (new-server 8080 "/joy/hello" (default-handler "Hello Cleveland"))
+  @(promise))
+
