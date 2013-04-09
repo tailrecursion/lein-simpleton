e7de55db3994f37cc71fea55b5679d383b108984
fogus <mefogus@gmail.com>
2013-04-09 08:56:06 -0400

    v1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-simpleton "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject lein-simpleton "1.0.0"
+  :description "A simple Leiningen plugin for serving files in a local directory."
+  :url "http://www.github.com/fogus/lein-simpleton"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
